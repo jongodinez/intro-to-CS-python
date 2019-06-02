a11beca48625fad36f97e1cf7f08f0e4e7a07971
jongodinez <jonathanjgodinez@gmail.com>
2019-06-01 20:49:53 -0700

    exercise update

---
+++ lesson2exercise.py
@@ -0,0 +1,7 @@
+print('Calculate the base and height of a triangle!')
+base = input('Enter the base of the triangle: ')
+height = input('Enter the height of the triangle: ')
+
+area = int(base) * int(height)
+
+print(area, 'is the area of the triangle.')
---
+++ lesson3exercise.py
@@ -0,0 +1,15 @@
+num = float(input('Enter a number: '))
+
+if (int(num) % 2 == 0):
+
+    if int(num) == 0:
+
+        print('Zero')
+
+    else:
+
+        print('Positive number')
+
+else:
+
+    print('Negative number')
